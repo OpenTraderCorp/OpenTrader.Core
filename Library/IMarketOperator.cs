2897ad59c1fac722c3c92ebff80842ce59c7a8cd
Leonardo Ferreira <index.xp@gmail.com>
2020-01-09 19:29:25 -0300

    Created the foundation of IMarketOperator

---
+++ Library/IMarketOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public interface IMarketOperator : IDisposable
+    {
+        Task<Quote> GetQuote(string symbol);
+        Task<IEnumerable<Quote>> GetQuotes(IEnumerable<string> symbols);
+    }
+}
