7d9bc8ae3a649b3c3a251cf08a844db07bc60009
Leonardo Ferreira <index.xp@gmail.com>
2020-01-17 16:39:35 -0300

    Started working on the platform Created the Core project which will hold implementations and the base class for the operation

---
+++ Core/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Library;
+
+namespace Core
+{
+    public class Platform
+    {
+        public IEnumerable<IAutonomousAgent> Agents
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public Task<IAutonomousAgent> LoadAutonomousAgent<T>() where T : IAutonomousAgent
+        {
+            return null;
+        }
+    }
+}
