1382a456b148988f8157ea083da04f7a40cd7432
Leonardo Ferreira <index.xp@gmail.com>
2020-01-20 16:59:18 -0300

    Started implementing AutonomousAgent launch

---
+++ Core/OperationalAgentInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using Library;
+
+namespace Core
+{
+    internal class OperationalAgentInfo
+    {
+        internal IAutonomousAgent Agent { get; set; }
+
+        internal CancellationTokenSource CancellationTokenSource { get; set; }
+
+        internal Guid UniqueID { get; set; }
+    }
+}
--- Core/Platform.cs
+++ Core/Platform.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Library;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Core
 {
     public class Platform
     {
+        private IServiceCollection _serviceCollection;
+
+        internal List<OperationalAgentInfo> RunningAgents { get; set; }
+
+
+        /// <summary>
+        /// Lists the Autonomous Agents that are being handled by this instance
+        /// </summary>
         public IEnumerable<IAutonomousAgent> Agents
         {
             get
             {
-                return null;
+                return RunningAgents.Select(i => i.Agent);
             }
         }
 
-        public Task<IAutonomousAgent> LoadAutonomousAgent<T>() where T : IAutonomousAgent
+        /// <summary>
+        /// Loads a implementation of IAutonomousAgent, instantiate it and makes
+        /// it ready to run
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public Task<IAutonomousAgent> LoadAutonomousAgent<T>() where T : class, IAutonomousAgent
         {
             return null;
         }
